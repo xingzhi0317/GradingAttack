e9bb32b1b3b282f6fd21fe43660f0a99f152c330
RegiaYoung <ryang379@connect.hkust-gz.edu.cn>
2026-07-26 10:26:25 +0000

    chore: use local ModelScope Llama path

--- scripts/run_qwen3_gcg_defense_full.sh
+++ scripts/run_qwen3_gcg_defense_full.sh
@@ -5,8 +5,8 @@ cd /home/ruijia/wxz/GradingAttack-gh
 
 CONDA=/home/ruijia/miniconda3/bin/conda
 
-env CUDA_VISIBLE_DEVICES=0 "$CONDA" run -n moe311 \
+"$CONDA" run -n moe311 \
   python main.py configs/GCG-Qwen3-4B-Instruct-local-attention-sharpening.yaml
 
-env CUDA_VISIBLE_DEVICES=0 "$CONDA" run -n moe311 \
+"$CONDA" run -n moe311 \
   python main.py configs/GCG-Qwen3-4B-Instruct-local-hijacking-suppression.yaml
